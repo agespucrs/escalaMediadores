d5a0565471401673fa693bdf81baa7d5bc205d2d
Gian Carlo <gian.luciano@acad.pucrs.br>
2016-05-09 17:58:08 -0300

    Arrumando a estrutura doalgoritmo e criando classe EscalaMediador

--- src/br/ages/model/Escala.java
+++ src/br/ages/model/Escala.java
@@ -4,25 +4,18 @@ import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
+import br.ages.escala.dao.EscalaDAO;
 import br.ages.exception.PersistenciaException;
 import br.ages.mediador.dao.MediadorDAO;
 
 public class Escala {
 
-	public class MediadorE{
-		
-		public Mediador mediador;
-		public AreaConhecimento areaManha;
-		public AreaConhecimento areaAlmoco;
-		public AreaConhecimento areaTarde;
-	
-	}
-	
 	Date data;
-	private MediadorE[] escalaMediadores;
-	private MediadorDAO dao = new MediadorDAO();
+	private EscalaMediador[] escalaMediadores;
+	private MediadorDAO mediadorDao = new MediadorDAO();
+	private EscalaDAO escalaDao = new EscalaDAO();
 	
-	public MediadorE[] gerarEscala(Date data) throws PersistenciaException, SQLException{
+	public EscalaMediador[] gerarEscala(Date data) throws PersistenciaException, SQLException{
 		this.data = data;
 		gerarMediadorE();
 		gerarAreaManha();
@@ -33,12 +26,11 @@ public class Escala {
 	
 	public void gerarMediadorE() throws PersistenciaException, SQLException{
 		
-		List<Mediador> mediadoresAtivos = dao.listaMediadoresAtivos(); 
-		escalaMediadores = new MediadorE[mediadoresAtivos.size()];
+		List<Mediador> mediadoresAtivos = mediadorDao.listaMediadoresAtivos(); 
+		escalaMediadores = new EscalaMediador[mediadoresAtivos.size()];
 		for (int i = 0; i < escalaMediadores.length; i++) {
 			escalaMediadores[i].mediador = mediadoresAtivos.get(i);
 		}
-		
 	}
 	
 	public void gerarAreaManha(){
---
+++ src/br/ages/model/EscalaMediador.java
@@ -0,0 +1,10 @@
+package br.ages.model;
+
+public class EscalaMediador {
+	
+	public Mediador mediador;
+	public AreaConhecimento areaManha;
+	public AreaConhecimento areaAlmoco;
+	public AreaConhecimento areaTarde;
+
+}
