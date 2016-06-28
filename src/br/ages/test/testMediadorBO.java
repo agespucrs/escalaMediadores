348838eaaf87b5f7bc6cc45d320e161becf5f240
15204007 <15204007@W3070620.portoalegre.pucrsnet.br>
2016-06-27 21:03:00 -0300

    Testes

--- src/br/ages/test/testMediadorBO.java
+++ src/br/ages/test/testMediadorBO.java
@@ -1,71 +1,56 @@
-	package br.ages.test;
+package br.ages.test;
 
-	import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-	import java.sql.Date;
-	import java.sql.SQLException;
+import java.sql.SQLException;
+import java.util.Date;
 
-	import org.junit.Before;
-	import org.junit.BeforeClass;
-	import org.junit.Test;
-	import org.junit.runner.RunWith;
-	import org.mockito.Mock;
-	import org.mockito.Mockito;
-	import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
-	import com.mysql.fabric.xmlrpc.base.Data;
-
-	import br.ages.exception.NegocioException;
-	import br.ages.exception.PersistenciaException;
+import br.ages.exception.NegocioException;
+import br.ages.exception.PersistenciaException;
 import br.ages.mediador.bo.MediadorBO;
 import br.ages.mediador.dao.MediadorDAO;
-import br.ages.model.AreaConhecimento;
-    import br.ages.model.Mediador;
-    import br.ages.model.Pavimento;
-	import br.ages.model.Status;
-	import br.ages.model.Tipo;
-	import junit.framework.Assert;
+import br.ages.model.Mediador;
+import br.ages.model.Status;
+import br.ages.model.Tipo;
 
+@RunWith(MockitoJUnitRunner.class)
+public class testMediadorBO {
 
-	@RunWith(MockitoJUnitRunner.class)
-	public class testMediadorBO {
-		
-		Mediador mediador = new Mediador();
-		 int idMediador;
-		 String cpf;
-		 String matricula;
-		 String nome;
-		 String email;
-		 Tipo tipoMediador;
-		 Status statusMediador;
-		 Date dataCadastro;
-		
-		
-		@Mock
-		private MediadorBO mediadorBO;
-		private MediadorDAO mediadorMockDAO;
-			
-		@Before
-		public void setUp(){	
-					mediadorBO = new MediadorBO();
-					mediadorMockDAO = new MediadorDAO();
-		}
-		
-		@Test
-		public void testCadastraArea() throws PersistenciaException, SQLException, NegocioException, ClassNotFoundException{
-			Mediador mediador = new Mediador(6, "85434345665", "56756","Laura" , "Laura@gmail.com", Tipo.DOIS, Status.ATIVO, null);
-			Mockito.when(mediadorMockDAO.cadastrarMediador(mediador)).thenReturn(80);
-			assertTrue(mediadorBO.cadastraMediador(mediador) == mediador.getIdMediador());
-					
-		}
-		
-		public void testValidaMediador() throws NegocioException, SQLException{
-			Mediador mediador = new Mediador(6, "85434345665", "56756","Laura" , "Laura@gmail.com", Tipo.DOIS, Status.ATIVO, null);
-			Mockito.when(mediadorBO.validaMediador(mediador)).thenReturn(true);
-			assertTrue(mediadorBO.validaMediador(mediador));
-		}
-			
+	Mediador mediador = new Mediador();
+	Date hoje = new Date();
+
+	@Mock
+	private MediadorBO mediadorBO;
+	private MediadorDAO mediadorMockDAO;
+
+	@Before
+	public void setUp() {
+		mediadorMockDAO = new MediadorDAO();
+		mediadorBO = new MediadorBO();
+		mediadorBO.setMediadorDAO(mediadorMockDAO);
+
+		// Mediador mediador = new Mediador(15, "02753400004",
+		// "222222222","Alana", "alana@gmail.com", Tipo.DOIS, Status.ATIVO,
+		// hoje);
 
 	}
 
+	@Test
+	public void testCadastraMediador()
+			throws  NegocioException, ClassNotFoundException, PersistenciaException, SQLException {
+		mediador = new Mediador(15, "02753453004", "11111111", "Alan", "alan@gmail.com", Tipo.DOIS, Status.ATIVO, hoje);
+		Mockito.when(mediadorMockDAO.cadastrarMediador(mediador)).thenReturn(15);
+		int idMediadorCriado = mediadorBO.cadastraMediador(mediador);
 
+		assertTrue(idMediadorCriado == mediador.getIdMediador());
+
+	}
+
+}
