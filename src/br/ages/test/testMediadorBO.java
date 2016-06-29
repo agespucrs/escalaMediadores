60da2236469a9ce8edd5b750a327f4f6c3de16b8
15204007 <15204007@W3070620.portoalegre.pucrsnet.br>
2016-06-29 17:58:27 -0300

    testes MediadorBO

--- src/br/ages/test/testMediadorBO.java
+++ src/br/ages/test/testMediadorBO.java
@@ -3,6 +3,7 @@ package br.ages.test;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.Date;
 
 import org.junit.Before;
@@ -51,6 +52,52 @@ public class testMediadorBO {
 
 		assertTrue(idMediadorCriado == mediador.getIdMediador());
 
+	}
+	
+	public void testValidaMediador() throws NegocioException, SQLException{
+		Mediador mediador = new Mediador(15, "02753400004", "2222","Alan" , "Alan@gmail.com", Tipo.DOIS, Status.ATIVO, hoje);
+		assertTrue(mediadorBO.validaMediador(mediador));
+	}
+		
+	@Test
+	public void testEditaMediador() throws PersistenciaException, NegocioException, SQLException{
+		Mediador mediador = new Mediador(15, "02753400004", "2222","Alan" , "Alan@gmail.com", Tipo.DOIS, Status.ATIVO, hoje);
+		Mockito.when(mediadorMockDAO.editaMediador(mediador)).thenReturn(true);
+	}
+	
+	@Test
+	public void testRemoveMediador() throws PersistenciaException, SQLException, NegocioException {
+		Mediador mediador = new Mediador(15, "02753400004", "2222","Alan" , "Alan@gmail.com", Tipo.DOIS, Status.ATIVO, hoje);
+		Mockito.when(mediadorMockDAO.removeMediador(mediador.getIdMediador())).thenReturn(true);
+
+	}
+	
+	@Test
+	public void testPesquisaMediadorNome() throws PersistenciaException, SQLException, ClassNotFoundException, NegocioException {
+		Mediador mediador = new Mediador(15, "02753400004", "2222","Alan" , "Alan@gmail.com", Tipo.DOIS, Status.ATIVO, hoje);
+		Mockito.when(mediadorMockDAO.pesquisarMediadorPorNome(mediador.getNome())).thenReturn(mediador);
+		assertTrue(mediador == mediadorBO.pesquisarMediadorPorNome(mediador.getNome()));
+	}
+	
+	@Test
+	public void testPesquisaMediadorId() throws PersistenciaException, ClassNotFoundException, NegocioException, ParseException, SQLException {
+		Mediador mediador = new Mediador(15, "02753400004", "2222","Alan" , "Alan@gmail.com", Tipo.DOIS, Status.ATIVO, hoje);
+		Mockito.when(mediadorMockDAO.pesquisarMediadorPorId(mediador.getIdMediador())).thenReturn(mediador);
+		assertTrue(mediador == mediadorBO.pesquisarMediadorPorId(mediador.getIdMediador()));
+	}
+	
+	@Test
+	public void testPesquisaMediadorCpf() throws PersistenciaException, ClassNotFoundException, NegocioException, ParseException, SQLException {
+		Mediador mediador = new Mediador(15, "02753400004", "2222","Alan" , "Alan@gmail.com", Tipo.DOIS, Status.ATIVO, hoje);
+		Mockito.when(mediadorMockDAO.pesquisarMediadorPorCpf(mediador.getCpf())).thenReturn(mediador);
+		assertTrue(mediador == mediadorBO.pesquisarMediadorPorCpf(mediador.getCpf()));
+	}
+	
+	@Test
+	public void testPesquisaMediadorMatricula() throws PersistenciaException, ClassNotFoundException, NegocioException, ParseException, SQLException {
+		Mediador mediador = new Mediador(15, "02753400004", "2222","Alan" , "Alan@gmail.com", Tipo.DOIS, Status.ATIVO, hoje);
+		Mockito.when(mediadorMockDAO.pesquisarMediadorPorMatricula(mediador.getMatricula())).thenReturn(mediador);
+		assertTrue(mediador == mediadorBO.pesquisarMediadorPorMatricula(mediador.getMatricula()));
 	}
 
 }
